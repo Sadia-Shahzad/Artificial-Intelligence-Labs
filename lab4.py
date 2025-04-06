f6726616dda3a41bac0a2bfd70046d56b11317cf
Sadia-Shahzad <sadiashahzad3147@gmail.com>
2025-04-07 00:57:42 +0500

    exceptions also handled

--- lab4.py
+++ lab4.py
@@ -3,8 +3,7 @@
 
 class Stack:
    
-    def __init__(self,size):
-        self.size=size
+    def __init__(self):
         self.noe=0
         self.list=[]
 
@@ -15,28 +14,75 @@ class Stack:
         return self.noe==0
     
     def peek(self):
-        return self.list[self.noe-1]
+        if (self.noe-1 >=0):
+            return self.list[self.noe-1]
+        else:
+            raise ValueError
     
     def push(self,ele):
         self.list+=[ele]    #python lists cannot add values on the indexes which do not exist instead we merge them with new lists
         self.noe+=1
 
     def pop(self):
-        deleted=self.list[self.noe-1]
-        self.noe-=1
-        return deleted
+        if (self.noe-1 < 0):
+            raise ValueError
+        else:
+            deleted=self.list[self.noe-1]
+            self.noe-=1
+            return deleted
     
 
-s1=Stack(10)
+s1=Stack()
 
 
 s1.push(10)
 s1.push(20)
 s1.push(30)
-try:
-    print("Popped",s1.pop())
-except empty_stack as es: 
 
-print("Top item",s1.peek())
+try:
+    popped=s1.pop()
+   
+except ValueError :
+    print("stack is already empty") 
+else:
+    print("Popped value is",popped)
+
+
+
+try:
+    print("Top item",s1.peek())
+except ValueError:
+    print("Empty stack cannot be peeked")
+
+
+try:
+    popped=s1.pop()
+   
+except ValueError :
+    print("stack is already empty") 
+else:
+    print("Popped value is",popped)
+
+try:
+    popped=s1.pop()
+   
+except ValueError :
+    print("stack is already empty") 
+else:
+    print("Popped value is",popped)
+
+try:
+    popped=s1.pop()
+   
+except ValueError :
+    print("stack is already empty") 
+else:
+    print("Popped value is",popped)
+
+try:
+    print("Top item",s1.peek())
+except ValueError:
+    print("Empty stack cannot be peeked")
+
 print("Is stack empty?",s1.is_empty())
 print("Current size is ",s1.total_elements())
